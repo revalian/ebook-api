5f4b6e8fbcc0789252096af9b29beac0e71728b3
chairil taufan <revalian2@gmail.com>
2021-09-28 22:37:33 +0700

    challenge

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,9 +19,17 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+// Routes Books
 Route::get('books',[App\Http\Controllers\BookController::class, 'index']);
 Route::post('books',[App\Http\Controllers\BookController::class, 'store']);
 Route::get('books/{id}',[App\Http\Controllers\BookController::class, 'show']);
 Route::put('books/{id}',[App\Http\Controllers\BookController::class, 'update']);
 Route::delete('books/{id}',[App\Http\Controllers\BookController::class, 'destroy']);
+
+// Routes Authors
+Route::get('authors',[App\Http\Controllers\AuthorController::class, 'index']);
+Route::post('authors',[App\Http\Controllers\AuthorController::class, 'store']);
+Route::get('authors/{id}',[App\Http\Controllers\AuthorController::class, 'show']);
+Route::put('authors/{id}',[App\Http\Controllers\AuthorController::class, 'update']);
+Route::delete('authors/{id}',[App\Http\Controllers\AuthorController::class, 'destroy']);
+
