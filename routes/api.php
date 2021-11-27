5ba2a940ca3b4aae763b9c931b979840525c19cb
chairil taufan <revalian2@gmail.com>
2021-11-27 13:03:22 +0700

    auth

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,18 +19,27 @@ use App\Http\Controllers\AuthorController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['middleware' => ['auth:sanctum']], function (){
 
-// Routes Books
-Route::get('books',[App\Http\Controllers\BookController::class, 'index']);
-Route::post('books',[App\Http\Controllers\BookController::class, 'store']);
-Route::get('books/{id}',[App\Http\Controllers\BookController::class, 'show']);
-Route::put('books/{id}',[App\Http\Controllers\BookController::class, 'update']);
-Route::delete('books/{id}',[App\Http\Controllers\BookController::class, 'destroy']);
+        // Route User dan Logout
+        Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+        Route::post('/me', [App\Http\Controllers\AuthController::class, 'me']);
 
-// Routes Authors
-Route::get('authors',[App\Http\Controllers\AuthorController::class, 'index']);
-Route::post('authors',[App\Http\Controllers\AuthorController::class, 'store']);
-Route::get('authors/{id}',[App\Http\Controllers\AuthorController::class, 'show']);
-Route::put('authors/{id}',[App\Http\Controllers\AuthorController::class, 'update']);
-Route::delete('authors/{id}',[App\Http\Controllers\AuthorController::class, 'destroy']);
+        // Routes Books
+        Route::get('books',[App\Http\Controllers\BookController::class, 'index']);
+        Route::post('books',[App\Http\Controllers\BookController::class, 'store']);
+        Route::get('books/{id}',[App\Http\Controllers\BookController::class, 'show']);
+        Route::put('books/{id}',[App\Http\Controllers\BookController::class, 'update']);
+        Route::delete('books/{id}',[App\Http\Controllers\BookController::class, 'destroy']);
 
+        // Routes Authors
+        Route::get('authors',[App\Http\Controllers\AuthorController::class, 'index']);
+        Route::post('authors',[App\Http\Controllers\AuthorController::class, 'store']);
+        Route::get('authors/{id}',[App\Http\Controllers\AuthorController::class, 'show']);
+        Route::put('authors/{id}',[App\Http\Controllers\AuthorController::class, 'update']);
+        Route::delete('authors/{id}',[App\Http\Controllers\AuthorController::class, 'destroy']);
+});
+
+// Routes Auth Baru
+Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
